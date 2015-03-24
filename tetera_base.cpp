40e6bf70d02a87b50b8b2cfdb867184e96ee8dd3
Ludwin Hernández <alduvi11@gmail.com>
2015-03-24 03:06:38 -0600

    Se agrega script de tetera base

---
+++ tetera_base.cpp
@@ -0,0 +1,92 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+
+void init(void)
+{
+
+
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0,  (GLsizei) w, (GLsizei) h);
+// Activamos la matriz de proyeccion.
+    glMatrixMode(GL_PROJECTION);
+// "limpiamos" esta con la matriz identidad.
+    glLoadIdentity();
+// Usamos proyeccion ortogonal
+    glOrtho(-300, 300, -300, 300, -300, 300);
+// Activamos la matriz de modelado/visionado.
+    glMatrixMode(GL_MODELVIEW);
+// "Limpiamos" la matriz
+    glLoadIdentity();
+
+
+
+}
+
+void display(void)
+{
+
+// "Limpiamos" el frame buffer con el color de "Clear", en este
+// caso negro.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode( GL_MODELVIEW_MATRIX );
+    glLoadIdentity();
+
+// Rotacion de 25 grados en torno al eje x
+    glRotated(25.0, 1.0, 0.0, 0.0);
+// Rotacion de -30 grados en torno al eje y
+    glRotated(-30.0, 0.0, 1.0, 0.0);
+// Dibujamos una "Tetera" y le aplico el material
+    glPushMatrix();
+//setMaterial
+
+    glutSolidTeapot(125.0);
+
+    glFlush();
+}
+
+
+int main(int argc, char **argv)
+{
+// Inicializo OpenGL
+    glutInit(&argc, argv);
+
+// Activamos buffer simple y colores del tipo RGB
+    glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH);
+
+// Definimos una ventana de medidas 800 x 600 como ventana
+// de visualizacion en pixels
+    glutInitWindowSize (800, 600);
+
+// Posicionamos la ventana en la esquina superior izquierda de
+// la pantalla.
+    glutInitWindowPosition (0, 0);
+
+// Creamos literalmente la ventana y le adjudicamos el nombre que se
+// observara en su barra de titulo.
+    glutCreateWindow ("Tetera");
+
+// Inicializamos el sistema
+    init();
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
